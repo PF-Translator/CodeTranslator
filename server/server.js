5a7a7c4ebff94f319318e821293ee73c0cca3e86
mp-04 <89947171+mp-04@users.noreply.github.com>
2022-03-19 17:26:46 -0400

    finished backend skeleton

--- server/server.js
+++ server/server.js
@@ -1,19 +1,36 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = 3000;
 //require controller that we setup here
-const router = ''
+const apiRouter = require('./routes/api.js');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('client'));
-app.use(express(json()));
 
+// handle all request to api
+app.use('/api', apiRouter);
 
-//insert global handler
+// error handler for bad requests
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+})
 
+//global handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'global error handler: unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred'}
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+})
 
-app.listen(port, () => {
-  console.log(`Always listenong on port: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Always listening on port: ${PORT}...`);
 })
 
 module.exports = app;
