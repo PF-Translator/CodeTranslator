029d53ebc86ac0ee2acfb61dc7b4c7de69b2333a
Myles Tsutsui <myles.tsutsui@gmail.com>
2022-03-21 12:36:13 -0700

    Completed userVerify and createUser controllers, working on redirecting to main after login.

--- server/controllers/quizController.js
+++ server/controllers/quizController.js
@@ -9,26 +9,61 @@ const quizController = {};
 //get request for logins
 
 
+ /* 
+ //res.locals.data =
+    {
+        userId:
+        learnLanguage:
+        learnLanguageTable:
+        knownLanguage:
+        knownLanguageTable:
+    }
+*/
 
+// {
+
+//   learnLanguageTable:
+      
+//   knownLanguageTable:
+// }
 
 
 //grab lesson row based on the user (or the userID)
-//
-quizController.getLessons = (req, res, next) => {
-    //query users table to get their learn_language and known_language  
-    selectString = 'SELECT learn_language a';
-    // from user known language table join the user learn_language table 
-    fromString = 'FROM user u RIGHT OUTER JOIN lesson l ';
+quizController.getLessons = async (req, res, next) => {
 
-    const queryString;
-    db.query(queryString).then((data) => {
+  try{
+  // grab known language and learn language as variables from the front end
+  // create variables for each: learn and known
+  //the following two variables are strings
+  const learnLang = res.locals.data.learn_language;
+  const knownLang = res.locals.data.known_language;
 
-    
+  //query users table to get their learn_language and known_language  
+  learnQueryString = `SELECT * FROM ${learnLang}`;
+  // from user known language table join the user learn_language table 
+  knownQueryString = `SELECT * FROM ${knownLang}`;
+
+  // invoking database query utilizing learnQueryString  
+  //asigning the async invocation to a variable called learn
+  let learn = await db.query(learnQueryString) 
+    //if we find a match to our query, store data response under a lable
+  // else return and console log err message not found
+  res.locals.data.learn_languageTable = learn.rows;
+
+  let known = await db.query(knownQueryString)
+
+  res.locals.data.known_languageTable = known.rows;
+  return next();
+  }catch (error) {
+    return next({
+      log: 'quiz.Controller.getLesons query failed;',
+      status: 422,
+      message: { err: error },
     })
-    //Left outer join => everything 
+  }
 }
  
-  //route to get user
+
 
   
 
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,34 +1,109 @@
+const { user } = require('pg/lib/defaults');
 const db = require('../database.js');
 
 const userController = {};
 
 userController.verifyUser = (req, res, next) => {
+  //check to make sure input is valid
   if(typeof req.body !== 'object' || !req.body.username || !req.body.password) {
-    next('userController.verifyUser: invalid input')
+    next({
+      log: 'invalid input',
+      message: { err: 'invalid input'}
+    })
   } else {
-    //do stuff
+    //query string
+    let str = 'SELECT * FROM users WHERE username = $1';
+    let arr = [req.body.username];
+    // query the database to get user info
+    db.query(str, arr, (err, response) => {
+      //handle query error if there is one
+      if(err) {
+        next({
+          log: 'userController.verifyUser query failed',
+          message: { err: err }
+        })
+      } else {
+        //grab response row when matching username
+        const user = response.rows[0];
+        //handle incorrect password
+        if(user.password !== req.body.password) {
+          next({
+            log: 'invalid password',
+            status: 401,
+            message: { err: 'invalid password'}
+          })
+          //if password matches, pass to next middleware funciton
+        } else {
+          res.locals.data = {
+            username: user.username,
+            user_id: user.user_id,
+            known_language: user.known_language,
+            learn_language: user.learn_language
+          }
+          next();
+        }
+      }
+    })
   }
 };
 
 userController.createUser = (req, res, next) => {
-  if(typeof req.body !== 'object' || !req.body.username || !req.body.password || !req.body.email) {
+ console.log(req.body)
+  if(typeof req.body !== 'object' || !req.body.username || !req.body.password || !req.body.email || !req.body.known_language || !req.body.learn_language) {
+   
     next('userController.creatUser: invalid input')
   } else {
-    str = `INSERT INTO users(username, password, email)
-    VALUES($1, $2, $3)`
-    arr = [req.body.username, req.body.password, req.body.email];
+    //query string
+    const str = `INSERT INTO users(username, password, known_language, learn_language, email)
+    VALUES($1, $2, $3, $4, $5)`
+    const arr = [req.body.username, req.body.password, req.body.known_language, req.body.learn_language, req.body.email];
+    //query the database to add user
     db.query(str, arr, (err, response) => {
+      //handle query error if there is one
       if(err) {
+        // console.log(req.body);
         next({
           log: 'userController.createUser query failed',
           message: {err: err }
         })  
       } else {
-        //do something
+        next();
 
       }
     })
   }
 };
+
+/*
+userController.deleteUser = (req, res, next) => {
+//grab username from req.body.username --- potentially user_id?
+const {username} = req.body.username;
+//query db to find user to be deleted
+const deleteQuery = `DELETE FROM USERS WHERE username = ${username}`;
+if (err) {
+  return next({
+    log: 'userController.deleteUser query failed',
+    message: {err: err}
+  })
+} else {
+  return next();
+}
+}
+
+userController.updateUser = (req, res, next) => {
+  //grab username from req.body.username --- potentially username
+  const {username, learn_language, known_language} = req.body.data;
+  //query db to isolate user's details
+  const updateLearnQuery = `UPDATE users SET learn_language = ${learn_language} WHERE username = ${username}`
+  //update details
+  db.query(updateLearnQueary) -------> left off here before lunch
+  //throw err if need be
+  //go to next piece of middleware
+  const updateKnownQuery = `UPDATE users SET `
+  //update details
+  //throw err if need be
+  //go to next piece of middleware
+}
+*/
 
 module.exports = userController;
